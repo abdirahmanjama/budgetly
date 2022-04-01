79f53f69d267c44112d70c0c4ae4818e53b80e9c
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-01 18:32:37 +0100

    finish tsx code for budgetcard

--- src/components/BudgetCard.tsx
+++ src/components/BudgetCard.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Card, ProgressBar } from "react-bootstrap";
+import { Card, ProgressBar, Button, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils/utils";
 
 interface Props {
   name: string;
   amount: number;
   max: number;
+  grey: boolean;
 }
 
-export default function BudgetCard({ name, amount, max }: Props) {
+export default function BudgetCard({ name, amount, max, grey }: Props) {
   return (
     <>
       <Card>
@@ -29,6 +30,12 @@ export default function BudgetCard({ name, amount, max }: Props) {
             max={max}
             now={amount}
           />
+          <Stack direction="horizontal" gap={2} className="mt-4">
+            <Button variant="outline-primary" className="ms-auto">
+              Add Expense
+            </Button>
+            <Button variant="outline-secondary">View Expenses</Button>
+          </Stack>
         </Card.Body>
       </Card>
     </>
