420562a5001312dde44e7ed691f7abf72352cf72
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-01 17:36:18 +0100

    populate budgetcard and add interface

--- src/components/BudgetCard.tsx
+++ src/components/BudgetCard.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-export default function BudgetCard() {
-  return <div>BudgetCard</div>;
+interface Props {
+  name: string;
+  amount: number;
+  max: number;
+}
+
+export default function BudgetCard({ name, amount, max }: Props) {
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <div>{name}</div>
+            <div>{amount}</div>
+            <div>{max}</div>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
